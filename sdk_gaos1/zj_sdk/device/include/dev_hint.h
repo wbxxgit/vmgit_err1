37432fae2842ac38e12f3394d57b9d2ca01734e2
wbxxgit <329744064@qq.com>
2020-09-24 20:45:35 +0800

    err1,again

---
+++ sdk_gaos1/zj_sdk/device/include/dev_hint.h
@@ -0,0 +1,23 @@
+#ifndef DEV_HINT_H_
+#define DEV_HINT_H_
+
+
+#define DEVVOICE_QL_START       "/mnt/config/aac/qlink_start.g711a"
+#define DEVVOICE_QL_SUCCEED     "/mnt/config/aac/qlink_success.g711a"
+#define DEVVOICE_QL_FAIL        "/mnt/config/aac/qlink_failure.g711a"
+#define DEVVOICE_QL_QROK1       "/mnt/config/aac/qrcode_ok1.aac"
+
+#define DEVVOICE_TRISETNET_QR       "/mnt/config/aac/qrcode_ok1.aac"
+#define DEVVOICE_TRISETNET_QLINK       "/mnt/config/aac/qrcode_ok1.aac"
+
+#define DEVVOICE_POWERRESET       "/mnt/config/aac/powerReset.aac"
+
+
+
+int dev_hintSoundP(char *filePath);
+
+#endif
+
+
+
+
